11c3205115324648547eaba2c46fd7f827afad26
Uchenna99 <uchennaagbu@gmail.com>
2025-08-14 21:11:54 +0100

    About us done

--- src/components/Button1.tsx
+++ src/components/Button1.tsx
@@ -1,12 +1,14 @@
 
 interface Props {
     text: string;
+    onClick: ()=>void;
 }
 
-const Button1 = ({ text }:Props) => {
+const Button1 = ({ text, onClick }:Props) => {
   return (
-    <button className="bg-primary text-background px-3 py-2 rounded-md cursor-pointer hover:bg-secondary 
-        transition-all duration-300 text-nowrap font-medium">
+    <button className="bg-primary text-background max-2xs:text-sm px-3 py-2 rounded-md cursor-pointer hover:bg-secondary 
+        transition-all duration-300 text-nowrap font-medium"
+        onClick={onClick}>
         {text}
     </button>
   )
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,9 +2,12 @@ import { motion } from "framer-motion";
 import { useEffect, useState } from "react";
 import Button1 from "../components/Button1";
 import FeaturesCard from "../components/FeaturesCard";
+import { useNavigate } from "react-router-dom";
+import AboutUsCard from "../components/AboutUsCard";
 
 
 const HomePage = () => {
+  const navigate = useNavigate();
   const [scrolled, setScrolled] = useState(false);
   const [scrolled2, setScrolled2] = useState(false);
   const features = [
@@ -13,6 +16,11 @@ const HomePage = () => {
     {icon:3, title:'Smart Logistics Optimization', description:'AI-powered route planning for the fastest, safest, and most efficient delivery every time.'},
     {icon:4, title:'Emergency Response Supply', description:'On-demand drone dispatch for urgent medical needs in critical situations.'},
     {icon:5, title:'Remote Area Access', description:'Reaching underserved regions with timely deliveries, overcoming terrain challenges.'},
+  ]
+  const about = [
+    {text:"Preventing avoidable casualties through faster emergency drugs delivery."},
+    {text:"We're on a mission to revolutionize medical logistics, speeding up critical drug deliveries to save more lives every day."},
+    {text:"Our drone network ensures swift and safe transportation of emergency drugs, reducing response times dramatically."}
   ]
   
   useEffect(() => {
@@ -32,55 +40,101 @@ const HomePage = () => {
     return () => window.removeEventListener('scroll', handleScroll);
   }, [scrolled]);
 
+  const handleClick = ()=>{
+    navigate('')
+  };
+
 
   return (
     <>
-      <div className="relative pt-[calc(100vh-100px)] flex flex-col items-center ">
+      <div className="relative pt-[calc(100vh-120px)] xs:pt-[calc(100vh-100px)] flex flex-col items-center ">
 
         {/* Hero section */}
         <div className="fixed top-0 left-0 w-full h-screen bg-primary flex justify-center">
           <div className="w-full h-full xl:max-w-[1280px]"></div>
         </div>
 
+
         {/* Rest of the page */}
-        <motion.div className={`relative z-10 bg-background p-5 h-[5000px] flex flex-col items-center gap-20`} 
-          initial={{width:800, maxWidth:'100%'}}
-          animate={scrolled2? {width:'100%'} : {}}
-          transition={{duration:0.4, ease:'easeInOut'}}>
-          
-          <div className="w-full flex justify-center">
-            <div className="w-[600px] max-w-full flex justify-between items-center gap-5">
-              <h4 className="text-text text-xl font-semibold">
-                MEDICAL SUPPLIES, <br />
-                DRONE DELIVERED 
-                <span className="text-accent"> ON TIME</span>
-              </h4>
+        <div className="relative z-10 w-full flex flex-col items-center">
+          <motion.div className={`bg-background p-5 flex flex-col items-center gap-14`} 
+            initial={{width:800, maxWidth:'100%'}}
+            animate={scrolled2? {width:'100%'} : {}}
+            transition={{duration:0.4, ease:'easeInOut'}}>
+            
+            <div className="w-full flex justify-center">
+              <div className="w-[600px] max-w-full flex justify-between items-center gap-5">
+                <div className="">
+                  <h4 className="text-text text-xl max-2xs:text-base font-semibold">
+                    MEDICAL SUPPLIES,
+                    
+                  </h4>
+                  <div className="flex gap-0 xs:gap-2 max-xs:flex-col">
+                    <h4 className="text-text text-xl max-2xs:text-base font-semibold text-nowrap">
+                      DRONE DELIVERED
+                    </h4>
+                    <h4 className="text-accent text-xl max-2xs:text-base font-semibold text-nowrap">
+                      ON TIME
+                    </h4>
+                  </div>
+                </div>
 
-              <Button1 text="GET STARTED" />
+                <Button1 text="GET STARTED" onClick={handleClick} />
+              </div>
+            </div>
+
+            <div className="w-full xl:w-[1240px] md:px-5 flex flex-col">
+              <div className="w-full grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 row-auto gap-6 py-12">
+                {
+                  features.map((feature, index)=>(
+
+                    <motion.div className="flex justify-center" key={index}
+                      initial={{scale:0.5, opacity:0}}
+                      animate={scrolled2? {scale:1, opacity:1} : {}}
+                      transition={{duration:0.4, ease:'easeInOut', delay:0.1}}>
+
+                      <FeaturesCard 
+                        icon={feature.icon}
+                        title={feature.title}
+                        description={feature.description}
+                      />
+                    </motion.div>
+                  ))
+                }
+              </div>
+            </div>
+
+          </motion.div>
+
+
+          {/* More about us section */}
+          <div className="w-full flex justify-center bg-white">
+            <div className="w-full xl:w-[1240px] flex flex-col gap-6 py-12 px-5 md:px-10">
+
+              <div className="flex justify-center">
+                <h4 className="text-text text-xl font-bold">
+                  MORE ABOUT US
+                </h4>
+              </div>
+
+              <div className="w-full overflow-x-scroll pb-2">
+                <div className="min-w-fit flex gap-6">
+                  {
+                    about.map((card, index)=>(
+
+                      <AboutUsCard 
+                        key={index}
+                        text={card.text}
+                      />
+                    ))
+                  }
+                </div>
+              </div>
+
             </div>
           </div>
 
-          <div className="w-full xl:w-[1240px] md:px-5 flex flex-col ">
-
-            <div className="w-full grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 row-auto gap-5">
-              {
-                features.map((feature, index)=>(
-
-                  <motion.div className="flex justify-center" key={index}>
-                    <FeaturesCard 
-                      icon={feature.icon}
-                      title={feature.title}
-                      description={feature.description}
-                    />
-                  </motion.div>
-                ))
-              }
-
-            </div>
-
-          </div>
-
-        </motion.div>
+        </div>
 
       </div>
     </>
