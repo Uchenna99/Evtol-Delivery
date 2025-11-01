33530126b548696afcc6a9ea7a2366e571494258
Uchenna99 <uchennaagbu@gmail.com>
2025-11-01 11:15:52 +0100

    testimonial section

--- src/components/sections/Testimonials.tsx
+++ src/components/sections/Testimonials.tsx
@@ -16,17 +16,18 @@ const Testimonials = () => {
         </div>
 
 
-        <div className="w-full max-w-[800px] bg-white rounded-2xl p-10 shadow-xl flex items-center gap-7">
+        <div className="w-full max-w-[800px] bg-white border border-gray-100 rounded-2xl p-10 shadow-xl flex flex-col 
+            md:flex-row items-center gap-7">
 
-            <div className="min-w-28 min-h-28 rounded-full bg-blue-100"></div>
+            <div className="min-w-28 min-h-28 rounded-full bg-blue-100 shadow-lg"></div>
 
             <div className="flex flex-col gap-5">
-                <p className="text-text text-xl italic">
+                <p className="text-text text-xl italic max-md:text-center">
                     "Reliable and fast! We depend on Evtol for emergency vaccine deliveries. The 24/7 availability has 
                     been crucial for our rural clinic operationss"
                 </p>
 
-                <div className="flex items-center text-amber-400">
+                <div className="flex items-center text-amber-400 max-md:justify-center">
                     {
                         Array.from({length:5}, (_,i)=> i + 1).map(()=>(
                             <Star size={18} fill="gold"/>
@@ -34,7 +35,7 @@ const Testimonials = () => {
                     }
                 </div>
 
-                <div className="">
+                <div className="flex flex-col items-center md:items-start">
                     <p className="text-primary text-2xl font-semibold">
                         Dr. James Okon
                     </p>
