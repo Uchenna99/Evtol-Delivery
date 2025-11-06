d5d3d9bab559d39523c80b5e714b36406345e2e7
Uchenna99 <uchennaagbu@gmail.com>
2025-11-06 15:51:40 +0100

    adding dashboard

--- src/pages/dashboard/DashboardHome.tsx
+++ src/pages/dashboard/DashboardHome.tsx
@@ -1,4 +1,5 @@
-import { BadgeDollarSign, Clock, PlaneTakeoff, ShieldCheck, User } from "lucide-react";
+import { BadgeDollarSign, BriefcaseMedical, Clock, PlaneTakeoff, ShieldCheck, User } from "lucide-react";
+import { Link } from "react-router-dom";
 
 
 
@@ -10,7 +11,7 @@ const DashboardHome = () => {
         {icon: <ShieldCheck size={20} />, value: "Verified", label: 'Account Status', colour: 'bg-primary'},
     ];
   return (
-    <div className="w-full flex flex-col gap-7 px-5">
+    <div className="w-full flex flex-col gap-7 p-5">
 
         <div className="w-full bg-gradient-to-br from-primary via-primary to-secondary rounded-xl 
             flex items-center justify-between p-5">
@@ -43,6 +44,53 @@ const DashboardHome = () => {
                         <p className="text-text text-sm">
                             {stat.label}
                         </p>
+                    </div>
+                ))
+            }
+        </div>
+
+
+        <div className="w-full flex flex-col gap-2 p-5 bg-white border border-gray-100 shadow-sm rounded-xl">
+
+            <div className="w-full flex justify-between items-center mb-2">
+                <p className="text-lg font-semibold">
+                    Recent Orders
+                </p>
+                <Link to={''} className="text-secondary font-semibold hover:text-primary hover:underline
+                    transition-all duration-200">
+                    View all
+                </Link>
+            </div>
+
+            {
+                Array.from({length:4}, (_, index)=>(
+                    <div className="w-full bg-gray-100 flex justify-between rounded-xl p-4" key={index}>
+
+                        <div className="flex items-center gap-3">
+                            <div className="w-11 aspect-square rounded-md grid place-items-center text-primary bg-primary/20">
+                                <BriefcaseMedical size={20}/>
+                            </div>
+                            <div className="">
+                                <p className="text-text text-sm font-semibold">
+                                    Vaccines
+                                </p>
+                                <p className="text-text/70 text-sm">
+                                    RiverCare Clinic
+                                </p>
+                                <p className="text-xs text-text/70">
+                                    7 hours ago
+                                </p>
+                            </div>
+                        </div>
+
+                        <div className="flex flex-col items-center gap-1">
+                            <p className="text-text text-sm font-semibold">
+                                N25,000
+                            </p>
+                            <div className="h-6 flex items-center px-3 rounded-xl bg-green-100 text-green-800 text-xs">
+                                Delivered
+                            </div>
+                        </div>
                     </div>
                 ))
             }
